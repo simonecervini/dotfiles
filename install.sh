cc32403b079c2b2312730f610331784f542550f7
Simone Cervini <sc.cervini@gmail.com>
2025-05-19 00:24:42 +0200

    Add custom Ghostty theme

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@
 cp .zshrc ~/.zshrc
 cp .tmux.conf ~/.tmux.conf
 rm -rf ~/.oh-my-zsh/custom && cp -R .oh-my-zsh/custom ~/.oh-my-zsh/custom
-cp ghostty $HOME/Library/Application\ Support/com.mitchellh.ghostty/config
+cp ghostty/config $HOME/Library/Application\ Support/com.mitchellh.ghostty/config
+mkdir -p ~/.config/ghostty/themes
+cp ghostty/custom-theme $HOME/.config/ghostty/themes/custom-theme
 
 zsh -i -c 'omz reload'
