85e31e19daa97a51452aea407f1ad3837e0aa506
Simone Cervini <sc.cervini@gmail.com>
2025-10-05 17:55:37 +0200

    Add VSCode/Cursor settings

--- install.sh
+++ install.sh
@@ -6,5 +6,7 @@ rm -rf ~/.oh-my-zsh/custom && cp -R .oh-my-zsh/custom ~/.oh-my-zsh/custom
 cp ghostty/config $HOME/Library/Application\ Support/com.mitchellh.ghostty/config
 mkdir -p ~/.config/ghostty/themes
 cp ghostty/custom-theme $HOME/.config/ghostty/themes/custom-theme
+cp cursor/settings.json $HOME/Library/Application\ Support/Cursor/User/settings.json
+cp cursor/keybindings.json $HOME/Library/Application\ Support/Cursor/User/keybindings.json
 
 zsh -i -c 'omz reload'
---
+++ scripts-sh/import-cursor-settings.sh
@@ -0,0 +1,2 @@
+cp $HOME/Library/Application\ Support/Cursor/User/settings.json ./cursor/settings.json
+cp $HOME/Library/Application\ Support/Cursor/User/keybindings.json ./cursor/keybindings.json
